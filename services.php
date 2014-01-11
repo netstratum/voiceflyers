4e800613aeb8e1f3aa0a9c8e7d8f6e1187071de9
Shamis Shukoor <shamis.shukoor@visualiq.com>
2014-01-11 23:43:12 +0530

    Adding product and pricing info

--- services.php
+++ services.php
@@ -11,59 +11,6 @@
 					</ul>
 				</div>
 			</div>
-		<div class="about-box">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-4">
-							<div class="about-us-text">
-								<h3>WHY VOICE FLYERS</h3>
-								<div class="innner-box">
-									<img alt="" src="upload/about.jpg">
-									<p><span>With more</span> advertising companies than the actual number of existing brands, it would be genuine to question the need for one more new entrant into creating brand awareness and reinforcement. Here is where Voice Flyers has a USP that distinguishes our self from the other advertisement firms. Here at Voice Flyers, we concentrate our focus on delivering real One to One ads to the customers. Unlike the ads played on a radio or a television which are primarily one to many ads, delivered to the universal set of all listeners or viewers, the ads served by Voice Flyers is focused on one listener at a time. Hence these ads are more customer centric, communicating on a personal note with the customer without any disturbances in between. Also, since it is the need of the customer to wait for the commencement of the call, the customer hence maintains his attention on the ads much more than a television or a radio ad . Moreover, since we utilize the dead time between the process of dial and call connect and hence subconsciously engage the customers into the ads, it ensures real one to one communication that guarantees the complete focus and attention of the listeners, provided the ads are engaging and customized for this utility. Hence, the ads served in voice flyers would be more customer centric than the ones played on a radio.</p>
-									
-								</div>
-							</div>
-						</div>
-
-						<div class="col-md-4">
-							<div class="skills-progress">
-								<h3>Our Skills</h3>
-								<p>WordPress Development <span>71%</span></p>
-								<div class="meter nostrips wp">
-									<span style="width: 254px;"></span>
-								</div>
-								<p>Photoshop <span>85%</span></p>
-								<div class="meter nostrips photoshop">
-									<span style="width: 304px;"></span>
-								</div>
-								<p>HTML5/CSS3 <span>76%</span></p>
-								<div class="meter nostrips html">
-									<span style="width: 272px;"></span>
-								</div>
-								<p>Ruby on Rails <span>53%</span></p>
-								<div class="meter nostrips ruby">
-									<span style="width: 190px;"></span>
-								</div>
-								<p>Social Marketing <span>69%</span></p>
-								<div class="meter nostrips marketing">
-									<span style="width: 247px;"></span>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-md-4">
-							<div class="about-us-text">
-								<h3>INTERPHASE & ANALYSIS</h3>
-								<div class="innner-box">
-									<img alt="" src="upload/about.jpg">
-									<p><span>A proper</span> mapping of the ads played, relative to each client is done by the software, designed so as to enable the clients to get a proper tracking of the ads together with providing exclusivity of the ads relative to the end customer. The software is programmed in such a way that it ensures that the same ads are not repeatedly streamed to the same customers consecutively. This ensures that the ads reach out t the most number of mutually exclusive distinct listeners. The frequency in which the ads are to be delivered to a single customer can be decided by the client as per the customization norms by selecting a package that is in accordance with the desired end result of the client. In any case, a detailed tracking of the ads played over the mentioned time period is sent to the clients. Hence the analysis and mapping of the ads delivered can be constantly accessed by the clients which ensures transparency and enables feedback.</p>
-									
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
 			<div class="pricing-box">
 				<div class="container">
 					<h3>Our Pricings</h3>
@@ -72,8 +19,8 @@
 						<div class="col-md-4">
 							<ul class="pricing-table basic">
 								<li class="title">
-									<p>Basic</p>
-									<span>$10,99/month</span>
+									<p>Gold (5 seconds)</p>
+									<span>Brand Reinforcement Ads</span>
 								</li>
 								<li>
 									<p>10GB Disk Space</p>
@@ -99,8 +46,8 @@
 						<div class="col-md-4">
 							<ul class="pricing-table standard">
 								<li class="title">
-									<p>Standard</p>
-									<span>$29,99/month</span>
+									<p>Diamond (10 seconds)</p>
+									<span>Product/Service Intro Ads</span>
 								</li>
 								<li>
 									<p>50GB Disk Space</p>
@@ -126,8 +73,8 @@
 						<div class="col-md-4">
 							<ul class="pricing-table basic">
 								<li class="title">
-									<p>Professional</p>
-									<span>$50,99/month</span>
+									<p>Platinum (15 seconds)</p>
+									<span>Information Ads</span>
 								</li>
 								<li>
 									<p>Unlimited Disk Space</p>
