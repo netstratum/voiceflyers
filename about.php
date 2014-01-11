649147e8fabf5450ec7dbd70b771464bd6c12eb2
Shamis Shukoor <shamis.shukoor@visualiq.com>
2014-01-11 12:57:03 +0530

    About page content

--- about.php
+++ about.php
@@ -18,8 +18,20 @@
 							<div class="services-post">
 								<a class="services-icon1" href="#"><i class="fa fa-cogs"></i></a>
 								<div class="services-post-content">
-									<h4>COMPANY</h4>
-									<p>Voice Flyers is a company that is into new age marketing of products and services focusing on brand reinforcements synchronizing with the telecom sector. Headed by a group of young budding entrepreneurs, the company has its office at Cochin. The operations were launched in January 2014, providing customized services to different clients, who aims to create or reinforce the brand awareness of their products or services among the customers. The company takes constant efforts to improve the quality of its service by taking feedbacks from its clients and hence introducing the necessary changes to the software which acts as the brain of the system, hence continuously improving the effectiveness of the inter phase system...</p>
+									<h3>COMPANY</h3>
+									<p>Voice Flyers is a company that is into new age marketing of products and services focusing 
+										on brand reinforcements synchronizing with the telecom sector. Headed by a group of young 
+										budding entrepreneurs, the company has its office at Cochin. The operations were launched 
+										in January 2014, providing customized services to different clients, who aims to create or 
+										reinforce the brand awareness of their products or services among the customers. The 
+										company takes constant efforts to improve the quality of its service by taking feedbacks from 
+										its clients and hence introducing the necessary changes to the software which acts as the brain 
+										of the system, hence continuously improving the effectiveness of the inter phase system. The 
+										vision and mission of the company is to subconsciously reinforce the brand awareness of the 
+										products and services of our clients and hence reach out to its customers across the globe by 
+										providing mutual benefit to both. We at Voice Flyers welcome different business groups from 
+										across the globe to partner us in this new venture of new age marketing.
+									</p>
 								</div>
 							</div>
 						</div>
@@ -30,20 +42,69 @@
 							<div class="services-post">
 								<a class="services-icon2" href="#"><i class="fa fa-desktop"></i></a>
 								<div class="services-post-content">
-									<h4>WHAT WE DO</h4>
-									<p>Voice Flyers as a company has access to more than 1,00,000 registered users who are willing to avail our service and synchronize our service with their telecom system. The audio ad streams played between the end of the dial and beginning of the call is the time slot where Voice Flyers subconsciously reinforces the brands of the clients into the minds of the customers. Since these ads are short and precise, catering into a customized group, it enables us to serve the best ads to the most relevant customer group hence acquiring high levels of targeting, respective to each brand. The preciseness and the customization of the ads served ensure that the ads are never disturbing for a customer by never waiting him too long for a call to be connected...</p>
+									<h3>WHAT WE DO</h3>
+									<p>Voice Flyers as a company has access to more than 1,00,000 registered users who are willing to 
+										avail our service and synchronize our service with their telecom system. The audio ad streams 
+										played between the end of the dial and beginning of the call is the time slot where Voice Flyers 
+										subconsciously reinforces the brands of the clients into the minds of the customers. Since these 
+										ads are short and precise, catering into a customized group, it enables us to serve the best 
+										ads to the most relevant customer group hence acquiring high levels of targeting, respective 
+										to each brand. The preciseness and the customization of the ads served ensure that the ads 
+										are never disturbing for a customer by never waiting him too long for a call to be connected. 
+										Instead, the ad keeps him engaging through information or a jingle or a reinforcement quote 
+										which subconsciously drives the customer into the brands of our clients. The customers are 
+										intern provided benefits from Voice Flyers as a compensation for subscribing to the ad streams 
+										and hence both the parties are mutually benefitted in the process.
+									</p>
 								</div>
 							</div>
 						</div>
 					</div>
-					
+
 					<div class="row">
 						<div class="col-md-12">
 							<div class="services-post">
 								<a class="services-icon3" href="#"><i class="fa fa-book"></i></a>
 								<div class="services-post-content">
-									<h4>WHY VOICE FLYERS</h4>
-									<p>With more advertising companies than the actual number of existing brands, it would be genuine to question the need for one more new entrant into creating brand awareness and reinforcement. Here is where Voice Flyers has a USP that distinguishes our self from the other advertisement firms. Here at Voice Flyers, we concentrate our focus on delivering real One to One ads to the customers. Unlike the ads played on a radio or a television which are primarily one to many ads, delivered to the universal set of all listeners or viewers, the ads served by Voice Flyers is focused on one listener at a time. Hence these ads are more customer centric, communicating on a personal note with the customer without any disturbances in between...</p>
+									<h3>WHY VOICE FLYERS</h3>
+									<p>With more advertising companies than the actual number of existing brands, it would be 
+										genuine to question the need for one more new entrant into creating brand awareness and 
+										reinforcement. Here is where Voice Flyers has a USP that distinguishes our self from the 
+										other advertisement firms. Here at Voice Flyers, we concentrate our focus on delivering real 
+										One to One ads to the customers. Unlike the ads played on a radio or a television which are 
+										primarily one to many ads, delivered to the universal set of all listeners or viewers, the ads 
+										served by Voice Flyers is focused on one listener at a time. Hence these ads are more customer 
+										centric, communicating on a personal note with the customer without any disturbances in 
+										between. Also, since it is the need of the customer to wait for the commencement of the call, 
+										the customer hence maintains his attention on the ads much more than a television or a radio 
+										ad. Moreover, since we utilize the dead time between the process of dial and call connect 
+										and hence subconsciously engage the customers into the ads, it ensures real one to one 
+										communication that guarantees the complete focus and attention of the listeners, provided the 
+										ads are engaging and customized for this utility. Hence, the ads served in voice flyers would be 
+										more customer centric than the ones played on a radio.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<div class="services-post">
+								<a class="services-icon3" href="#"><i class="fa fa-book"></i></a>
+								<div class="services-post-content">
+									<h3>INTERPHASE &amp; ANALYSIS</h3>
+									<p>A proper mapping of the ads played, relative to each client is done by the software, designed so 
+										as to enable the clients to get a proper tracking of the ads together with providing exclusivity of 
+										the ads relative to the end customer. The software is programmed in such a way that it ensures 
+										that the same ads are not repeatedly streamed to the same customers consecutively. This 
+										ensures that the ads reach out t the most number of mutually exclusive distinct listeners. The 
+										frequency in which the ads are to be delivered to a single customer can be decided by the client 
+										as per the customization norms by selecting a package that is in accordance with the desired 
+										end result of the client. In any case, a detailed tracking of the ads played over the mentioned 
+										time period is sent to the clients. Hence the analysis and mapping of the ads delivered can be 
+										constantly accessed by the clients which ensures transparency and enables feedback.
+									</p>
 								</div>
 							</div>
 						</div>
